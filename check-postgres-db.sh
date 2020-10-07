a9ed28a142fbcaab5cafab81cf9298630fdf99a9
hohuyhoangbk <hohuyhoangbk@gmail.com>
2020-10-07 15:59:02 +0700

    Create check-postgres-db.sh

---
+++ check-postgres-db.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#echo $1
+database_name="ITViec_test$1"
+echo $database_name
+#sudo su - postgres -c "psql -lqt | cut -d \| -f 1|grep -qw ${database_name}"
+if sudo su - postgres -c "psql -lqt | cut -d \| -f 1|grep -qw ${database_name}"; then
+    echo "database exists"
+else
+    echo "create a new database"
+    sudo su - postgres <<EOF
+        psql -c "CREATE DATABASE \"${database_name}\""
+EOF
+    #sudo su - postgres -c 'psql create database \${database_name}'
+fi
